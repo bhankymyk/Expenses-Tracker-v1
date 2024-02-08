128973a7f0e2fbe6a837b6f46a279c715f5b7813
bhankymyk <bankole123michael@gmail.com>
2024-02-08 13:15:09 +0100

    Feat;files uploaded for deployment

---
+++ src/components/YourBalance.vue
@@ -0,0 +1,32 @@
+<template>
+    
+        <div class="row">
+            <div class="col bal">
+                <h4>Your Balance is </h4>
+                <h1 class="Balance">#{{ total }}</h1>
+            </div>
+        </div>
+
+</template>
+
+<script>
+    export default {
+        props: {
+    total: {
+      type: Number,
+      required: true
+    },
+  },
+    }
+</script>
+
+<style scoped>
+.bal{
+    text-align: center;
+}
+.Balance {
+    color: #198754;
+}
+
+
+</style>
